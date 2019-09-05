55bd83f3f64c039ed94a37129bbf7d5f4d4456de
Mustafa <hazim6163@gmail.com>
2019-09-05 05:11:02 +0200

    ceate the layout

--- app/src/main/java/com/example/booklist/MainActivity.java
+++ app/src/main/java/com/example/booklist/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.booklist;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        final SearchView searchView = (SearchView)findViewById(R.id.search_view);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.e(null, "onQueryTextSubmit: "+query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
